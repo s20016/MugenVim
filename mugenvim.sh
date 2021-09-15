ffd8f369ba642687a6ea9d5ebed60f1bdcc25d58
s20016 <s20016@std.it-college.ac.jp>
2021-09-15 18:33:11 +0900

    Test: Fix path error

--- mugenvim.sh
+++ mugenvim.sh
@@ -55,11 +55,11 @@ if [ ! -d $CONFIG_DIR ]; then
   sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
   cd $TMP_DIR && git clone https://github.com/s20016/MugenVim.git
-	cp $TMP_DIR/conf/init.vim $CONFIG_DIR/.
+	cp $TMP_DIR/MugenVim/conf/init.vim $CONFIG_DIR/.
   nvim +'PlugInstall --sync' +qa
-	cp $TMP_DIR/conf/setting.vim $CONFIG_DIR/.
-	cp $TMP_DIR/conf/mapping.vim $CONFIG_DIR/.
-	cp $TMP_DIR/conf/plugins.vim $CONFIG_DIR/.
+	cp $TMP_DIR/MugenVim/conf/setting.vim $CONFIG_DIR/.
+	cp $TMP_DIR/MugenVim/conf/mapping.vim $CONFIG_DIR/.
+	cp $TMP_DIR/MugenVim/conf/plugins.vim $CONFIG_DIR/.
   mkdir $SESSIO_DIR
   gruvbox_tab
   nvim +'source $CONFIG_DIR/init.vim' +q
